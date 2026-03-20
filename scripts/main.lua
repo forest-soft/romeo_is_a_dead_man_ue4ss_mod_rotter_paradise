43322d2b12d00eecf34b58cd27c9c1f8bc279ec7
森林ソフト <kimoto@forest-soft.net>
2026-03-21 01:22:26 +0900

    最初のチャプター用の調整を入れた。

--- scripts/main.lua
+++ scripts/main.lua
@@ -5,7 +5,7 @@ print("[Rotter Paradise] OK")
 
 --[[
 ■メモ
-・DT_EnemySetListのID
+・DT_EnemySetListのIDリスト
 TEST_TestZombies
 TEST_Walkers
 TEST_MultiTypeZombies
@@ -129,7 +129,7 @@ ExecuteInGameThread(function()
 			"GekkeoZombie",
 			"BirdZombie",
 			"Jelly",
-			--[[
+			
 			"VS_DeadBurg_WP_EnemySet_001",
 			"VS_DeadBurg_WP_EnemySet_002",
 			"VS_DeadBurg_WP_EnemySet_003",
@@ -158,9 +158,6 @@ ExecuteInGameThread(function()
 			"DeadBurg_1968_AroundCultArea_02",
 			"DeadBurg_1968_AroundCultArea_03",
 			"DeadBurg_1968_AroundCultArea_04",
-			--]]
-			"FirstRotter",
-			"WeakFirstRotter",
 			"Mantis",
 		}
 		for i, enemy_set_id in ipairs(target_enemy_set_id_list) do
@@ -172,7 +169,7 @@ ExecuteInGameThread(function()
 			
 			target_row.Enemy4 = "ESpawnAICharaID_Walker"
 			target_row.Enemy5 = "ESpawnAICharaID_Walker"
-			--[[
+			
 			target_row.Enemy6 = "ESpawnAICharaID_Walker"
 			target_row.Enemy7 = "ESpawnAICharaID_Walker"
 			target_row.Enemy8 = "ESpawnAICharaID_Walker"
@@ -190,11 +187,41 @@ ExecuteInGameThread(function()
 	end
 	
 	-- 敵書き換え(パレス・アテネ)
-	-- Fatal Errorでうまくいかない
+	-- DataTableを書き換えても反映されない。
 	--[[
 	local dt_dungeons_room_enemies_set = StaticFindObject("/Game/DataTable/Dungeons/DT_DungeonsRoomEnemiesSet.DT_DungeonsRoomEnemiesSet")
-	if dt_dungeons_room_enemies_set and dt_dungeons_room_enemies_set:IsValid() then
+	
+	if dt_dungeons_room_enemies_set:IsValid() then
 		dt_dungeons_room_enemies_set:ForEachRow(function(row_name, row_data)
+			print(row_data.Comment:ToString())
+			print(row_data.Enemies0.EnemyID)
+			print(row_data.Enemies1.EnemyID)
+			print(row_data.Enemies2.EnemyID)
+			print(row_data.Enemies3.EnemyID)
+			
+			if row_data.Enemies0.EnemyID ~= 0 then
+				row_data.Enemies0.EnemyID = 1
+			end
+			
+			if row_data.Enemies1.EnemyID ~= 0 then
+				row_data.Enemies1.EnemyID = 1
+			end
+			
+			if row_data.Enemies2.EnemyID ~= 0 then
+				row_data.Enemies2.EnemyID = 1
+			end
+			
+			if row_data.Enemies3.EnemyID ~= 0 then
+				row_data.Enemies3.EnemyID = 1
+			end
+
+			--[[
+			
+			row_data.Enemies1.EnemyID = 1
+			row_data.Enemies2.EnemyID = 1
+			row_data.Enemies3.EnemyID = 1
+			
+			--[[
 			row_data.Enemies0.EnemyID = "ESpawnAICharaID::ESpawnAICharaID_Walker"
 			row_data.Enemies0.AITag = "WalkOnlyRotterVisModernRotterVisClassicRotterVisCityHallRotterVisPolice"
 			row_data.Enemies0.Num = 1
@@ -210,11 +237,61 @@ ExecuteInGameThread(function()
 			row_data.Enemies3.EnemyID = "ESpawnAICharaID::ESpawnAICharaID_None"
 			row_data.Enemies3.AITag = "None"
 			row_data.Enemies3.Num = 0
-			
+			]]
+	--[[
 		end)
 	end
 	]]
 end)
+
+-- スポナーから出てくる敵を書き換える。
+NotifyOnNewObject("/Game/Blueprint/Object/DimensionEyeSpawnEnemy/BP_DimensionEyeSpawnEnemy.BP_DimensionEyeSpawnEnemy_C", function(target_object)
+	if target_object:IsValid() then
+	
+		-- 「mEnemyCharaIDs」がセットされるのを待ち、セットされたら書き換える。
+		local function patch()
+			local patch_target_object = target_object
+			
+			if not(patch_target_object:IsValid()) or patch_target_object.mEnemyCharaIDs[1] == 0 then
+				-- print("まだ初期化前？")
+				
+				ExecuteWithDelay(1, patch)
+				return
+			end
+			
+			--[[
+			print("パッチ前")
+			patch_target_object.mEnemyCharaIDs:ForEach(function(index, value)
+				print(index .. " = " .. value:get())
+			end)
+			]]
+			
+			-- 全ての敵をWalkerにしつつ、スポナーから出てくる敵の数を3倍にする。
+			-- print("パッチ実行:" .. patch_target_object.mEnemyCharaIDs[1])
+			local id_length = #patch_target_object.mEnemyCharaIDs
+			for i = 1 , id_length do
+				-- 「1」はWalker
+				-- 「4」はTomatoHead
+				patch_target_object.mEnemyCharaIDs[i] = 1
+				
+				patch_target_object.mEnemyCharaIDs[id_length + i] = 1
+				patch_target_object.mEnemyCharaIDs[(id_length * 2) + i] = 1
+			end
+			
+			--[[
+			print("パッチ後")
+			patch_target_object.mEnemyCharaIDs:ForEach(function(index, value)
+				print(index .. " = " .. value:get())
+			end)
+			]]
+			
+			-- 同時出現の上限数
+			patch_target_object.mEnemyCapacity = 10
+		end
+		ExecuteWithDelay(1, patch)
+	end
+end)
+
 
 
 --[[
@@ -224,8 +301,14 @@ MAPの出現情報を強制的にWalkerに書き換えて回避する。
 -- /Game/Blueprint/Enemy/BP_SevTargetPoint_EnemySet.BP_SevTargetPoint_EnemySet_C
 NotifyOnNewObject("/Game/Blueprint/Enemy/BP_SevTargetPoint_EnemySet.BP_SevTargetPoint_EnemySet_C", function(target_object)
 	if target_object:IsValid() then
-		-- print(target_object.mEnemySetID:ToString())
 		
+		-- FullNameチェック用
+		--[[
+		ExecuteWithDelay(100, function()
+			print(target_object:GetFullName())
+			print(target_object.mEnemySetID:ToString())
+		end)
+		]]
 		
 		-- local full_name_prefix = "BP_SevTargetPoint_EnemySet_C ";
 		-- local target_path = string.sub(target_object:GetFullName(), string.len(full_name_prefix))
@@ -236,6 +319,12 @@ NotifyOnNewObject("/Game/Blueprint/Enemy/BP_SevTargetPoint_EnemySet.BP_SevTarget
 		
 		
 		local city_hall_walker_patch_target_class_list = {
+			-- 最初のファーストロッター戦でエリア外にロッターが出てしまうので書き換えないようにする。
+			"/Game/Map/Cassini/OWL_CassiniSt/_Generated_/7ZBY7MXT8B2GRQC91JEGX9SAC.OWL_CassiniSt:PersistentLevel.BP_SevTargetPoint_EnemySet_C_UAID_7085C29403A8E48F02_c8fc790242ec4bde_2007637712",
+			"/Game/Map/Cassini/OWL_CassiniSt/_Generated_/7ZBY7MXT8B2GRQC91JEGX9SAC.OWL_CassiniSt:PersistentLevel.BP_SevTargetPoint_EnemySet_C_UAID_7085C29403A8E48F02_c8fc790242ec4bde_2010096713",
+			"/Game/Map/Cassini/OWL_CassiniSt/_Generated_/7ZBY7MXT8B2GRQC91JEGX9SAC.OWL_CassiniSt:PersistentLevel.BP_SevTargetPoint_EnemySet_C_UAID_7085C29403A8E48F02_c8fc790242ec4bde_2013145714",
+			
+			
 			"/Game/Map/RouteMap/CH_Juliet/OWL_Juliet/_Generated_/B219P6HYIUUZPN9XFZ1ULKKCK.OWL_Juliet:PersistentLevel.BP_SevTargetPoint_EnemySet_2_GEN_VARIABLE_BP_SevTargetPoint_EnemySet_C_CAT_0",
 			"/Game/Map/RouteMap/CH_Juliet/OWL_Juliet/_Generated_/B219P6HYIUUZPN9XFZ1ULKKCK.OWL_Juliet:PersistentLevel.BP_SevTargetPoint_EnemySet_2_GEN_VARIABLE_BP_SevTargetPoint_EnemySet_C_CAT_1",
 			"/Game/Map/RouteMap/CH_Juliet/OWL_Juliet/_Generated_/B219P6HYIUUZPN9XFZ1ULKKCK.OWL_Juliet:PersistentLevel.BP_SevTargetPoint_EnemySet_3_GEN_VARIABLE_BP_SevTargetPoint_EnemySet_C_CAT_0",
@@ -273,18 +362,21 @@ NotifyOnNewObject("/Game/Blueprint/Enemy/BP_SevTargetPoint_EnemySet.BP_SevTarget
 		
 		-- 「mEnemySetID」がセットされるのを待ち、セットされたら書き換える。
 		local function patch()
-			-- local path_target_object = StaticFindObject(target_path)
-			local path_target_object = target_object
-			if not(path_target_object:IsValid()) or path_target_object.mEnemySetID:ToString() == "None" then
+			-- local patch_target_object = StaticFindObject(target_path)
+			local patch_target_object = target_object
+			if not(patch_target_object:IsValid()) or patch_target_object.mEnemySetID:ToString() == "None" then
 				print("まだ初期化前？")
 				
 				ExecuteWithDelay(1, patch)
 				return
 			end
 			
-			print("パッチ実行:" .. path_target_object.mEnemySetID:ToString())
-			path_target_object.mEnemySetID = FName("TEST_TestZombies")
+			print("パッチ実行:" .. patch_target_object.mEnemySetID:ToString())
+			patch_target_object.mEnemySetID = FName("TEST_TestZombies")
 		end
 		ExecuteWithDelay(1, patch)
+		
+		
+		
 	end
 end)
